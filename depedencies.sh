8b30924c9eb84108b3eabccf5cfedc1fd620d1b6
cawaongit <sasha.martinhernandez@gmail.com>
2026-05-03 02:55:11 +0200

    Some automatisation with scripts

---
+++ depedencies.sh
@@ -0,0 +1,31 @@
+# OpenCV
+sudo apt install libopencv-dev
+
+
+# WiringPi
+sudo apt install git
+git clone https://github.com/WiringPi/WiringPi.git
+cd WiringPi
+
+# build the package
+./build debian
+mv debian-template/wiringpi_3.18_arm64.deb .
+
+# install it
+sudo apt install ./wiringpi_3.18_arm64.deb
+
+# remove repo
+cd ..
+rm -rf WiringPi/
+
+
+# pca9685
+git clone https://github.com/Reinbert/pca9685.git
+cd pca9685/src
+
+sudo make install
+
+mv pca9685.o ../../lib/
+
+cd ../../
+rm -rf pca9685/
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd build/
+cmake .. && make && ./SPider /dev/input/event5
